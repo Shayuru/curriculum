b30ac0ddc2286596c45642c5082976d272673569
Shayuru <shayuru@yahoo.com.mx>
2021-11-18 13:55:59 -0600

    Initial changes to support English & Spanish lang.

---
+++ src/app/models/default-info-es-MX.ts
@@ -0,0 +1,189 @@
+export var DefaultCurriculumInfoSpanish = {
+  name: 'Francisco Jardón Estrada',
+  title: 'Ingeniero de Software',
+  profile: [
+    'Apasionado de la tecnología, con amplia experiencia en el desarrollo de sistemas principalmente en el sector financiero como Ingeniero de software, Ingeniero full stack.',
+    'Sensible a las necesidades del usuario, analítico, disciplinado y con visión para la solución de problemas en el desarrollo de software.',
+    'Mi educación incluye la maestría en Ingeniería Informática en la Universidad Autónoma del Estado de México (UAEM).',
+    'Durante mi carrera he trabajado con una variedad de tecnologías y lenguajes de programación como: Java 2EE, Spring, Restful API, SOAP Webservices, IBM MQ, Message Broker, HTML, CSS, JavaScript, JSP, Angular/JS, Shell Scripting, PLSQL, T-SQL, Jenkins, Git/GitHub, WebSphere, Hibernate, Oracle, SQL Server, DB2, Android, .NET Framework (VB & C#), ASP .NET. ADO .NET y algunos otros.',
+  ],
+  education: {
+    grade: 'Maestría en Ingeniería Informática',
+    school: 'Universidad Autónoma del Estado de México (UAEM)',
+    date: '2003 - 2005',
+  },
+  contact: {
+    telegram: '@fcojardone',
+    email: 'francisco.jardon@outlook.com',
+    location: 'Toluca, Estado de México',
+    linkedin: 'http://www.linkedin.com/in/francisco-jardon-e-b4639930',
+    github: 'https://github.com/Shayuru',
+  },
+  languages: ['Inglés - Intermedio/Avanzado', 'Español - Nativo'],
+  courses: [
+    'Test Driven Development & Behavior Driven Development',
+    'Security Engineering on AWS',
+    'Developing on AWS',
+    'AWS Certified Solution Architect- Associate SAA-C02',
+  ],
+  softSkills: [
+    'Adaptabilidad al cambio',
+    'Pensamiento análitco/critico',
+    'Organización',
+    'Empatía',
+  ],
+  sdlcTypes: ['Scrum', 'SAFe', 'Cascada'],
+  hobbies: ['Fotografía', ' Senderismo ', 'Lectura', 'Escuchar música'],
+  experience: [
+    {
+      company: 'HSBC GLOBAL SERVICE CENTER MÉXICO (EDPM)',
+      location: 'TOLUCA, MEXICO',
+      date: '2015 a la Actualidad',
+      position: 'SOFTWARE ENGINEER & SECURITY CHAMPION',
+      tools:
+        'Java 2EE, Spring (MVC, IoC, Validation, Transactions, JDBC, JMS, Tasks, Scheduling, etc.), Restful API, SOAP Webservices, IBM MQ, Message Broker, HTML, CSS, JS, jQuery, Bootstrap, JSP, Angular/JS, Shell Scripting, PLSQL, Eclipse, STS, Gradle, Maven, Oracle, Android Studio, Linux Red Hat, Checkmarx, Netsparker, IQ Server, Nexus, Jenkins, Postman, VS Code, SoapUI, Git/GitHub, WebSphere, Liberty, Jira, Confluence',
+      description: [
+        {
+          activity:
+            'Revisión y remediación de hallazgos de seguridad en el desarrollo de las aplicaciones Web,Java, análisis: SAST, DAST y FOSS',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Asesorar a los ingenieros junior en el desarrollo seguro de software',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Desarrollo y mantenimiento de aplicaciones (backend y frontend) incluyendo actividades como analisis de requerimientos, diseño de solución, desarrollo, pruebs y post-impementación:',
+          subactivities: [
+            'Aplicación web para mantenimiento masivo de cuentas y contratos de nómina (identificación y clasificación) en los sistemas de mainframe (hasta 50 mil registros por solicitud)',
+            'Agenda web para integración de expediente de clientes, diseñada para completar la apertura de cuentas de nómina (contrato digital)',
+            'App para Android para ingegración de expediente, toma de fotos de documentos (INE, documento de residencia), OCR, toma de huelllas mediante 4F (identificación mediante foto)',
+          ],
+        },
+        {
+          activity:
+            'Generación de flujos en IBM Message Broker para el backend de SFE (aplicación de call center), para obtener información de cliente, cuentas, tarjetas del sistema core origen (mainframe)',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Planificación y seguimiento de actividades relacionadas al desarrollo de software como asignación de recursos, establecimiento de fechas, control de cambios, etc.',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Análisis y seguimiento para migración de aplicaciones web a AWS',
+          subactivities: [],
+        },
+        {
+          activity: 'Migracion de aplicaciones de Websphere a Liberty ',
+          subactivities: [],
+        },
+      ],
+    },
+    {
+      company: 'HSBC MÉXICO',
+      location: 'TOLUCA, MÉXICO',
+      date: '2007 - 2015',
+      position: 'Asesor de Sistemas (Desarollador mid-level)',
+      tools:
+        'Java 2EE, Spring (IoC, Validation, Transactions, JDBC), Hibernate, SOAP Webservices, IBM MQ, HTML, CSS, JS, JSP, Servlets, CGI, Shell Scripting, PLSQL, Oracle, DB2, Informix, Linux Suse, Rational Team Concert, WebSphere, .NET Framework (VB & C#), Maven',
+      description: [
+        {
+          activity:
+            'Desarrollo y mantenimiento de aplicaciones (backend y frontend) incluyendo actividades como analisis de requerimientos, diseño de solución, desarrollo, pruebs y post-impementación:',
+          subactivities: [
+            'Aplicación web de alta de cuentas y clientes de Nomina de forma masiva, hasta 8 mil cliente y cuentas al dia lo que representa unas 100 mil peticiones a los sistemas de mainframe, la concurrencia era un punto clave para mantener la estabilidad de mainframe (máximo 20 procesos en paralelo)',
+            'Portabilidad de Nómina interbancaria mediante aplicación web, proveer servicios web para diferentes canales (banca por internet y sucursales) en la apertura y consulta de la portabilidad (hasta 1.5 millones de peticiones al día), transferencia de dinero mediante el sistema electronico de pagos interbancarios SPEI (hasta 25 mil peticiones por proceso programado, 3 procesos al día) ',
+            'Aplicación web para sobretasas en inversiones de tasa fija/variable',
+            'Aplicación de escritorio para interactuar con la emulación de mainframe y actualizar información de clientes y cuentas',
+          ],
+        },
+        {
+          activity: 'Generación de documentación técnica y funcional',
+          subactivities: [],
+        },
+      ],
+    },
+
+    {
+      company: 'HSBC MÉXICO',
+      location: 'TOLUCA, MÉXICO',
+      date: '2004 - 2007',
+      position: 'SUPERVISOR DE PROYECTOS ESPECIALES/MEDICIÓN',
+      tools:
+        '.NET Framework (C# & VB), ASP .NET. ADO .NET, T-SQL, MS SQL Server 2000-2005/Integration Services, IIS, Visual Studio, Business Objects, Windows 2000/2003 Server',
+      description: [
+        {
+          activity:
+            'Análisis, seguimiento e implementación de los proyectos especiales de monitoreo de /ATM’s. Cuatro empleados me reportaban directamente.',
+          subactivities: [],
+        },
+        {
+          activity: 'Desarrollo de Programas:',
+          subactivities: [
+            'Aplicaciones de escritorio para notificación de fallas de ATM’s usando: Email, Celular, Sky, SNMP, SFTP',
+            'Aplicaciones de escritorio para limpieza de datos',
+            'Aplicaciones web en ASP .NET para reunir información de las sucursales, encuestas de satisfacción de cliente, seguimiento de proyectos (para ayudar a evaluar el programa de administración de desempeño)',
+          ],
+        },
+        {
+          activity:
+            'Migración de Web basado en Java (JSP y beans)  y el modelado de datos para IT Dashboard (contiendo KPI’s, Indicadores de Procesos Críticos, etc. realizados en Business Objects), en .NET y Websphere y patrón MVC (dos plataformas diferentes).',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Administración y mantenimiento de base de datos incluyendo acividades como:',
+          subactivities: [
+            'Modelado y creación de base datos, tablas, store procedures, vistas, ETLs (car y descarga de información hasta 2 millones de registros por día)',
+            'Control de acceso de usuarios',
+            'Respaldos',
+          ],
+        },
+        {
+          activity: 'Generación de documentación técnica y funcional',
+          subactivities: [],
+        },
+      ],
+    },
+    {
+      company: 'DISASTER RECOVERY SERVICES',
+      location: 'METEPEC, MÉXICO/CIUDAD DE MÉXICO',
+      date: '2003 - 2004',
+      position: 'DESARROLLADOR',
+      tools:
+        'VB, SQL Server 2000, HTML, JS, CSS, IIS, Crystal Reports/Business Objects, Windows 2000 Serve, Citrix',
+      description: [
+        {
+          activity:
+            'Desarrollo de aplicación de Inventarios considerando: uso de scanner de código de barras, capaz de agregar, borrar, modificar  y buscar registros, manejar y proponer tiempos de entrega y recolección de medios, visualización de reportes, control de acceso mediante usuario y contraseña',
+          subactivities: [],
+        },
+        {
+          activity: 'Generación de reportes de inventarios',
+          subactivities: [],
+        },
+        {
+          activity: 'Desarrollo de la pagina Web e intranet de la compañía',
+          subactivities: [],
+        },
+        {
+          activity: 'Diseño e implementación de la Base de datos en SQL Server',
+          subactivities: [],
+        },
+        {
+          activity:
+            'Recolección de información acerca de cintas de respaldo de  ING  Comercial América para migrar  su Base de Datos del inventario hacia Disaster Recovery',
+          subactivities: [],
+        },
+        {
+          activity: 'Generación de documentación técnica y funcional',
+          subactivities: [],
+        },
+      ],
+    },
+  ],
+};
