3a696d6df18dc1decb389f14ed171ff2eb04479e
Shayuru <shayuru@yahoo.com.mx>
2021-11-18 15:49:21 -0600

    Change in service to get the curriculum info. based on the lang.

--- src/app/services/curriculum.service.ts
+++ src/app/services/curriculum.service.ts
@@ -3,15 +3,22 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { utils } from '../../environments/utils';
+import { getLanguageParameter } from '../utils/common.utils';
 
 @Injectable()
 export class CurriculumService {
   headers: HttpHeaders = new HttpHeaders().set('x-api-key', utils.apiKey);
+  params: HttpParams;
 
-  params = new HttpParams().set('id', 1).set('lang', 'en');
   public apiPath: string = '/prod/v1/curriculum';
 
-  constructor(public _httpClient: HttpClient) {}
+  constructor(public _httpClient: HttpClient) {
+    const idCurriculum: number = 1;
+    const localeParam: string = getLanguageParameter(localStorage);
+    this.params = new HttpParams()
+      .set('id', idCurriculum)
+      .set('lang', localeParam);
+  }
 
   getCurriculInfo(): Observable<any> {
     let url = environment.apisBaseUrl.concat(this.apiPath);
